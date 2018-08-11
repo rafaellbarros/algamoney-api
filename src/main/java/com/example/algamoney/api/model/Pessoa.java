19714f9c2268b63ea8a428ab7ab82ce74821b297
Rafael Barros <rafaelbarros.df@gmail.com>
2018-08-11 01:31:34 -0300

    Criando classe entity Pessoa

---
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -0,0 +1,31 @@
+package com.example.algamoney.api.model;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "pessoa")
+@Data
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	
+	@NotNull
+	private String nome;
+	
+	@Embedded
+	private Endereco endereco;
+	
+	@NotNull
+	private Boolean ativo;
+
+}
