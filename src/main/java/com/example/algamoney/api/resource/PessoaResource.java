79acb4ee9ea6242b04e07220d7cc974c96b70d14
Rafael Barros <rafaelbarros.df@gmail.com>
2018-08-11 21:25:01 -0300

    Implementando remover

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,11 @@ public class PessoaResource {
 	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo){
 		Pessoa pessoaCodigo = pessoaRepository.findOne(codigo);
 		return pessoaCodigo != null ? ResponseEntity.ok(pessoaCodigo) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
 	}
 }
