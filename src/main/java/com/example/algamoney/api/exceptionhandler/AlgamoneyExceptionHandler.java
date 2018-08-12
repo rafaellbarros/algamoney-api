19ced3f79185111df6502874ddc2220748a26309
Rafael Barros <rafaelbarros.df@gmail.com>
2018-08-11 22:07:03 -0300

    Ajustando exceção

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -31,7 +31,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		String mensagemDsenvolvedor = ex.getCause().toString();
+		String mensagemDsenvolvedor = ex.getCause() != null ? ex.getCause().toString() : ex.toString();
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario,  mensagemDsenvolvedor));
 		
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
