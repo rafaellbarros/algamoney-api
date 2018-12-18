275d3f883c64a725ff2a5189de7211e8423a987b
Rafael Barros <rafaelbarros.df@gmail.com>
2018-12-17 23:53:47 -0200

    Nome do usuário no toke JWT

---
+++ src/main/java/com/example/algamoney/api/security/UsuarioSistema.java
@@ -0,0 +1,30 @@
+package com.example.algamoney.api.security;
+
+
+
+
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import com.example.algamoney.api.model.Usuario;
+
+import lombok.Getter;
+
+
+public class UsuarioSistema extends User {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Getter
+	private Usuario usuario;
+
+	public UsuarioSistema(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		this.usuario = usuario;
+	}
+
+
+}
