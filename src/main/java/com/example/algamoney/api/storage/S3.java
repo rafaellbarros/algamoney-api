ebe36b81902a5dd919b5741277a65457410cfc67
Rafael Barros <rafaelbarros.df@gmail.com>
2019-05-30 09:37:35 -0300

    :construction: 22.36 - Configurando URL do anexo

--- src/main/java/com/example/algamoney/api/storage/S3.java
+++ src/main/java/com/example/algamoney/api/storage/S3.java
@@ -72,7 +72,7 @@ public class S3 {
 	}
 	
 	public String configurarUrl(String objeto) {
-		return "\\\\" + property.getS3().getBucket() + 
+		return "//" + property.getS3().getBucket() + 
 				".s3.amazonaws.com/" + objeto;
 	}
 	
